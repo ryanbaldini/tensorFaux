c88540f9cef44230b8a9a048240443af32ac43c6
Ryan Baldini <ryan.baldini@seagate.com>
2017-09-28 00:02:30 -0600

    removed alignas(), not used

--- include/Node.h
+++ include/Node.h
@@ -14,11 +14,11 @@ struct Node
 	string name;
 	vector<int> dim;
 	int nValues;
-	alignas(32) double* values;				//computed values of the node; altered on forward pass
-	alignas(32) double* gradient;			//gradient on the values; altered on backward pass
+	double* values;				//computed values of the node; altered on forward pass
+	double* gradient;			//gradient on the values; altered on backward pass
 	int nParameters;
-	alignas(32) double* parameters;
-	alignas(32) double* parameterGradient;
+	double* parameters;
+	double* parameterGradient;
 	vector< Node* > parents;
 	vector< Node* > children;
 	Activation activate;
